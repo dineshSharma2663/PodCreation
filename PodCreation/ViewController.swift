cb88c5e087a3258dc9b12088ddd820dc2f34d8a2
dinesh sharma <dineshsharma@dineshs-Mac-mini.local>
2017-08-22 16:56:00 +0530

    first commit

---
+++ PodCreation/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  PodCreation
+//
+//  Created by dinesh sharma on 22/08/17.
+//  Copyright © 2017 dinesh sharma. All rights reserved.
+//
+
+import UIKit
+
+
+func podCreated() {
+    print("asdasd asda56dasd4ad")
+}
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
